2d5ed141fb49462f38ff0f093b9e130aa9fde4ff
nyt23 <153197020+nyt23@users.noreply.github.com>
2024-01-27 01:35:55 +0100

    update

--- inspirationQuote.js
+++ inspirationQuote.js
@@ -1,14 +1,16 @@
-document.getElementById('inspirationButton').addEventListener('click', async function() {
-    try {
-        const response = await fetch('https://api.quotable.io/random?tags=inspirational');
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        document.getElementById('quoteDisplay').innerText = `"${data.content}" - ${data.author}`;
-    } catch (error) {
+
+
+document.getElementById('inspirationButton').addEventListener('click', function() {
+    fetch('https://api.quotable.io/random?tags=inspirational')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('quoteDisplay').innerText = `"${data.content}" - ${data.author}`
+        })
+        .catch (error => {
         console.error('Error fetching quote: ', error);
         document.getElementById('quoteDisplay').innerText = 'Failed to fetch quote. Please try again';
-    }
+    })
+
 });
+
 
