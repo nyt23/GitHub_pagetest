5863082c8e01538d6f1e7dacb05f827d241f3925
nyt23 <153197020+nyt23@users.noreply.github.com>
2024-03-27 19:40:23 +0100

    update

--- inspirationQuote.js
+++ inspirationQuote.js
@@ -1,6 +1,9 @@
 
 
 document.getElementById('inspirationButton').addEventListener('click', function() {
+    const replaceText = "Here's your today's quote!"
+    document.getElementById('inspirationButton').replaceWith(replaceText);
+
     fetch('https://api.quotable.io/random?tags=inspirational')
         .then(response => response.json())
         .then(data => {
@@ -10,8 +13,8 @@ document.getElementById('inspirationButton').addEventListener('click', function(
         console.error('Error fetching quote: ', error);
         document.getElementById('quoteDisplay').innerText = 'Failed to fetch quote. Please try again';
     })
-    const replaceText = "Great! You've got your today's quote! Come back tomorrow!"
-    document.getElementById('inspirationButton').replaceWith(replaceText);
 });
+
+
 
 
